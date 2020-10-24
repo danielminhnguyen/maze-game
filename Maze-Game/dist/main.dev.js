37c40aea0e062435d88b3264d7ef1c7e38474bfb
hokkinxx <minhngyen68@hotmail.com>
2020-10-24 14:00:15 +1300

    add player

--- Maze-Game/dist/main.dev.js
+++ Maze-Game/dist/main.dev.js
@@ -1,7 +1,8 @@
 "use strict";
 
 //Build random Maze
-//Inspire from
+// TODO:
+//
 //#region Maze Generator
 // Variables
 var width = 10; //Canvas width
@@ -10,6 +11,7 @@ var height = 10; // Canvas height
 
 var startPos = [1, height];
 var endPos = [width, 1];
+var mazeTable;
 var color = {
   startCell: "green",
   finishCell: "purple",
@@ -19,8 +21,118 @@ var color = {
 
 var $ = function $(id) {
   return document.getElementById(id);
-}; //#region  Functions
+};
 
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
+var interval = 1; // var KeyboardHelper = { left: 37, up: 38, right: 39, down: 40 };
+//#region  Functions
+
+function keyDownHandler(e) {
+  if ("code" in e) {
+    switch (e.code) {
+      case "Unidentified":
+        break;
+
+      case "ArrowRight":
+      case "Right": // IE <= 9 and FF <= 36
+
+      case "KeyD":
+        rightPressed = true;
+        return;
+
+      case "ArrowLeft":
+      case "Left": // IE <= 9 and FF <= 36
+
+      case "KeyA":
+        leftPressed = true;
+        return;
+
+      case "ArrowUp":
+      case "Up": // IE <= 9 and FF <= 36
+
+      case "KeyW":
+        upPressed = true;
+        return;
+
+      case "ArrowDown":
+      case "Down": // IE <= 9 and FF <= 36
+
+      case "KeyS":
+        downPressed = true;
+        return;
+
+      default:
+        return;
+    }
+  }
+
+  if (e.keyCode == 39) {
+    rightPressed = true;
+  } else if (e.keyCode == 37) {
+    leftPressed = true;
+  }
+
+  if (e.keyCode == 40) {
+    downPressed = true;
+  } else if (e.keyCode == 38) {
+    upPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if ("code" in e) {
+    switch (e.code) {
+      case "Unidentified":
+        break;
+
+      case "ArrowRight":
+      case "Right": // IE <= 9 and FF <= 36
+
+      case "KeyD":
+        rightPressed = false;
+        return;
+
+      case "ArrowLeft":
+      case "Left": // IE <= 9 and FF <= 36
+
+      case "KeyA":
+        leftPressed = false;
+        return;
+
+      case "ArrowUp":
+      case "Up": // IE <= 9 and FF <= 36
+
+      case "KeyW":
+        upPressed = false;
+        return;
+
+      case "ArrowDown":
+      case "Down": // IE <= 9 and FF <= 36
+
+      case "KeyS":
+        downPressed = false;
+        return;
+
+      default:
+        return;
+    }
+  }
+
+  if (e.keyCode == 39) {
+    rightPressed = false;
+  } else if (e.keyCode == 37) {
+    leftPressed = false;
+  }
+
+  if (e.keyCode == 40) {
+    downPressed = false;
+  } else if (e.keyCode == 38) {
+    upPressed = false;
+  }
+}
 
 function createTable(w, h) {
   mazeWidth = w;
@@ -34,6 +146,8 @@ function createTable(w, h) {
     for (var row = 1; row <= mazeWidth; row++) {
       var mazeColumn = document.createElement("td");
       mazeColumn.setAttribute("id", "cell_".concat(row, "_").concat(col));
+      mazeColumn.style.width = "".concat(90 / width, "vmin");
+      mazeColumn.style.height = "".concat(90 / height, "vmin");
       mazeRow.appendChild(mazeColumn);
     }
 
@@ -63,18 +177,83 @@ function shuffle(array) {
   }
 
   return array;
+}
+
+function buildMaze() {
+  currentCell.positionX = startPos[0];
+  currentCell.positionY = startPos[1];
+  $(currentCell.id()).classList.add("visited"); // exit.path.push([currentCell.positionX, currentCell.positionY]); // current correct cell
+
+  exit.visited.push([currentCell.positionX, currentCell.positionY]); //visited cell
+
+  for (i = 0; i <= width * height; i++) {
+    // (function (i) {
+    //   setTimeout(function () {
+    // Notify when exit found
+    if (currentCell.positionX == endPos[0] && currentCell.positionY == endPos[1]) {
+      exit.solution = exit.path.slice(); //Make copy of the exit path
+
+      exit.found = true;
+      console.log("Exit Found");
+    } //  Check avaiable direction from current cell
+
+
+    currentCell.checkNeighbour(); //   action when reach dead end
+
+    if (currentCell.validNeighbour.length == 0) {
+      moveBack = exit.path[exit.path.length - 1];
+
+      if (moveBack != undefined) {
+        currentCell.positionX > moveBack[0] ? direction = "left" : direction = "right";
+        currentCell.positionY > moveBack[0] ? direction = "top" : direction = "bottom";
+        currentCell.positionX = moveBack[0];
+        currentCell.positionY = moveBack[1]; // exit.fullPath.push(direction);
+
+        currentCell.checkNeighbour();
+        exit.path.pop();
+        i--;
+      } else {
+        console.log("exit path is empty");
+      }
+    } else {
+      //   Filter avaiable direction of current cell for exit Path)
+      //Pick random an exit from the
+      randomPick = Math.floor(Math.random() * currentCell.validNeighbour.length);
+      exit.direction = currentCell.validNeighbour[randomPick]; // when weight direction doesn't include the path
+
+      if (exit.direction == undefined) {
+        exit.direction = currentCell.validNeighbour[0];
+      } else {
+        a = document.getElementById(currentCell.id());
+        a.style["border-".concat(exit.direction)] = "none";
+      } //  Continue to create deadend
+
+
+      exit.path.push([currentCell.positionX, currentCell.positionY]); // solution array
+      // break the wall in front
+
+      currentCell.move(exit.direction); //Move following direction
+
+      exit.visited.push([currentCell.positionX, currentCell.positionY]); //visited array
+
+      a = document.getElementById(currentCell.id());
+      a.classList.add("visited");
+      a.style["border-".concat(exit.directionOpposite())] = "none"; //break the wall behind
+    } //   }, interval * i);
+    // })(i);
+
+  }
+} // $(`cell_${startPos[0]}_${startPos[1]}`).style.backgroundColor = color.startCell;
+// $(`cell_${endPos[0]}_${endPos[1]}`).style.backgroundColor = color.finishCell;
+
+
+function showSolution() {
+  exit.solution.forEach(function (a) {
+    $("cell_".concat(a[0], "_").concat(a[1])).style.backgroundColor = color.path;
+  });
 } //#endregion End Function
 //#region  Objects
 
-
-var player = {
-  start: function start() {
-    this.positionX = startPos[0];
-    this.positionY = startPos[1];
-  },
-  positionX: null,
-  positionY: null
-}; //  Construct shortest Exit path
 
 var exit = {
   direction: null,
@@ -166,11 +345,129 @@ var currentCell = {
   id: function id() {
     return "cell_".concat(currentCell.positionX, "_").concat(currentCell.positionY);
   }
-}; //#endregion Object
-//  Creat blank table
+};
+var player = {
+  start: function start() {
+    this.positionX = startPos[0];
+    this.positionY = startPos[1];
+    this.icon = document.createElement("i");
+    this.icon.classList.add("fas");
+    this.icon.classList.add("fa-chess-king");
+    this.initiate = true;
+  },
+  positionX: null,
+  positionY: null,
+  icon: null,
+  totalMove: [],
+  availableMove: ["right", "left", "top", "bottom"],
+  validMove: [],
+  checkPath: function checkPath() {
+    this.validMove = [];
+    a = $("cell_".concat(player.positionX, "_").concat(player.positionY));
 
+    for (var _i = 0; _i <= 3; _i++) {
+      if (a.style["border-".concat(this.availableMove[_i])] == "none") {
+        this.validMove.push(this.availableMove[_i]);
+        console.log(this.availableMove[_i]);
+      }
+    }
+  },
+  show: function show() {
+    if (this.initiate) {
+      $("cell_".concat(player.positionX, "_").concat(player.positionY)).appendChild(this.icon);
+    } else {
+      console.log("Please Initiate Player");
+    }
+  },
+  hide: function hide() {
+    if (this.initiate) {
+      $("cell_".concat(player.positionX, "_").concat(player.positionY)).removeChild(this.icon);
+    } else {
+      console.log("Please Initiate Player");
+    }
+  },
+  move: function move(d) {
+    this.checkPath();
+
+    if (this.validMove.indexOf(d) >= 0) {
+      this.hide();
+
+      switch (d) {
+        case "top":
+          this.totalMove.push["top"];
+          this.positionY--;
+          upPressed = false;
+          break;
+
+        case "bottom":
+          this.totalMove.push["bottom"];
+          this.positionY++;
+          downPressed = false;
+          break;
+
+        case "left":
+          this.totalMove.push["left"];
+          this.positionX--;
+          leftPressed = false;
+          break;
+
+        case "right":
+          this.totalMove.push["right"];
+          this.positionX++;
+          rightPressed = false;
+          break;
+
+        default:
+          console("Invalid Move");
+          break;
+      }
+
+      this.show();
+      console.log(d);
+    }
+  }
+}; //#endregion Object
+
+function play() {
+  $("mazeContainer").innerHTML = "";
+  createTable(width, height);
+  buildMaze();
+  player.start();
+  player.show();
+} //  Creat blank table
+
+
+$("mazeContainer").innerHTML = "";
 createTable(width, height);
-player.start(); //  Shortest path to the destination
+buildMaze();
+showSolution(); //#endregion  End MAZE GENERATOR
+
+player.start();
+player.show();
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+checkscore = setInterval(function () {
+  if (rightPressed) {
+    player.move("right");
+  }
+
+  if (leftPressed) {
+    player.move("left");
+  }
+
+  if (upPressed) {
+    player.move("top");
+  }
+
+  if (downPressed) {
+    player.move("bottom");
+  }
+
+  if (player.positionX == endPos[0] && player.positionY == endPos[1]) {
+    clearInterval(checkscore);
+    alert("You Win");
+  }
+}, 100); //  Shortest path to the destination
 // let xmin = endPos[0] - startPos[0];
 // let ymin = endPos[1] - startPos[1];
 // Vertical distance
@@ -182,72 +479,4 @@ player.start(); //  Shortest path to the destination
 // // Randomize exitpath
 // exit.availablePath = shuffle(exit.availablePath);
 // initiate starting point
-
-currentCell.positionX = startPos[0];
-currentCell.positionY = startPos[1];
-$(currentCell.id()).classList.add("visited");
-exit.path.push([currentCell.positionX, currentCell.positionY]); // current correct cell
-
-exit.visited.push([currentCell.positionX, currentCell.positionY]); //visited cell
-
-for (i = 0; i <= width * height; i++) {
-  // Notify when exit found
-  if (currentCell.positionX == endPos[0] && currentCell.positionY == endPos[1]) {
-    exit.solution = exit.path.slice(); //Make copy of the exit path
-
-    exit.found = true;
-    console.log("Exit Found");
-  } //  Check avaiable direction from current cell
-
-
-  currentCell.checkNeighbour(); //   action when reach dead end
-
-  if (currentCell.validNeighbour.length == 0) {
-    moveBack = exit.path[exit.path.length - 1];
-
-    if (moveBack != undefined) {
-      currentCell.positionX > moveBack[0] ? direction = "left" : direction = "right";
-      currentCell.positionY > moveBack[0] ? direction = "top" : direction = "bottom";
-      currentCell.positionX = moveBack[0];
-      currentCell.positionY = moveBack[1]; // exit.fullPath.push(direction);
-
-      currentCell.checkNeighbour();
-      exit.path.pop();
-      i--;
-    } else {
-      console.log("exit path is empty");
-    }
-  } else {
-    //   Filter avaiable direction of current cell for exit Path)
-    // Check for direction
-    randomPick = Math.floor(Math.random() * currentCell.validNeighbour.length); //Pick random an exit from the
-
-    exit.direction = currentCell.validNeighbour[randomPick]; // when weight direction doesn't include the path
-
-    if (exit.direction == undefined) {
-      exit.direction = currentCell.validNeighbour[0];
-    } else {
-      a = document.getElementById(currentCell.id());
-      a.style["border-".concat(exit.direction)] = "none";
-    } //  Continue to create deadend
-
-
-    exit.path.push([currentCell.positionX, currentCell.positionY]); // solution array
-    // break the wall in front
-
-    currentCell.move(exit.direction); //Move following direction
-
-    exit.visited.push([currentCell.positionX, currentCell.positionY]); //visited array
-
-    a = document.getElementById(currentCell.id());
-    a.classList.add("visited");
-    a.style["border-".concat(exit.directionOpposite())] = "none"; //break the wall behind
-  }
-}
-
-exit.solution.forEach(function (a) {
-  $("cell_".concat(a[0], "_").concat(a[1])).style.backgroundColor = color.path;
-});
-$("cell_".concat(startPos[0], "_").concat(startPos[1])).style.backgroundColor = color.startCell;
-$("cell_".concat(endPos[0], "_").concat(endPos[1])).style.backgroundColor = color.finishCell; //#endregion  End MAZE GENERATOR
 //  check exit.found false then re run the program
--- Maze-Game/main.js
+++ Maze-Game/main.js
@@ -1,13 +1,14 @@
 //Build random Maze
-//Inspire from
+// TODO:
+//
 
 //#region Maze Generator
-
 // Variables
 let width = 10; //Canvas width
 let height = 10; // Canvas height
 let startPos = [1, height];
 let endPos = [width, 1];
+let mazeTable;
 const color = {
   startCell: "green",
   finishCell: "purple",
@@ -17,18 +18,110 @@ const color = {
 var $ = function (id) {
   return document.getElementById(id);
 };
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
+var interval = 1;
+// var KeyboardHelper = { left: 37, up: 38, right: 39, down: 40 };
 
 //#region  Functions
+function keyDownHandler(e) {
+  if ("code" in e) {
+    switch (e.code) {
+      case "Unidentified":
+        break;
+      case "ArrowRight":
+      case "Right": // IE <= 9 and FF <= 36
+      case "KeyD":
+        rightPressed = true;
+        return;
+      case "ArrowLeft":
+      case "Left": // IE <= 9 and FF <= 36
+      case "KeyA":
+        leftPressed = true;
+        return;
+      case "ArrowUp":
+      case "Up": // IE <= 9 and FF <= 36
+      case "KeyW":
+        upPressed = true;
+        return;
+      case "ArrowDown":
+      case "Down": // IE <= 9 and FF <= 36
+      case "KeyS":
+        downPressed = true;
+        return;
+      default:
+        return;
+    }
+  }
+
+  if (e.keyCode == 39) {
+    rightPressed = true;
+  } else if (e.keyCode == 37) {
+    leftPressed = true;
+  }
+  if (e.keyCode == 40) {
+    downPressed = true;
+  } else if (e.keyCode == 38) {
+    upPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if ("code" in e) {
+    switch (e.code) {
+      case "Unidentified":
+        break;
+      case "ArrowRight":
+      case "Right": // IE <= 9 and FF <= 36
+      case "KeyD":
+        rightPressed = false;
+        return;
+      case "ArrowLeft":
+      case "Left": // IE <= 9 and FF <= 36
+      case "KeyA":
+        leftPressed = false;
+        return;
+      case "ArrowUp":
+      case "Up": // IE <= 9 and FF <= 36
+      case "KeyW":
+        upPressed = false;
+        return;
+      case "ArrowDown":
+      case "Down": // IE <= 9 and FF <= 36
+      case "KeyS":
+        downPressed = false;
+        return;
+      default:
+        return;
+    }
+  }
+
+  if (e.keyCode == 39) {
+    rightPressed = false;
+  } else if (e.keyCode == 37) {
+    leftPressed = false;
+  }
+  if (e.keyCode == 40) {
+    downPressed = false;
+  } else if (e.keyCode == 38) {
+    upPressed = false;
+  }
+}
+
 function createTable(w, h) {
   mazeWidth = w;
   mazeHeight = h;
-  var mazeTable = document.createElement("table");
-  var mazeBody = document.createElement("tbody");
+  let mazeTable = document.createElement("table");
+  let mazeBody = document.createElement("tbody");
   for (let col = 1; col <= mazeHeight; col++) {
     var mazeRow = document.createElement("tr");
     for (let row = 1; row <= mazeWidth; row++) {
       var mazeColumn = document.createElement("td");
       mazeColumn.setAttribute("id", `cell_${row}_${col}`);
+      mazeColumn.style.width = `${90 / width}vmin`;
+      mazeColumn.style.height = `${90 / height}vmin`;
       mazeRow.appendChild(mazeColumn);
     }
     mazeBody.appendChild(mazeRow);
@@ -60,20 +153,85 @@ function shuffle(array) {
   return array;
 }
 
+function buildMaze() {
+  currentCell.positionX = startPos[0];
+  currentCell.positionY = startPos[1];
+
+  $(currentCell.id()).classList.add("visited");
+
+  // exit.path.push([currentCell.positionX, currentCell.positionY]); // current correct cell
+  exit.visited.push([currentCell.positionX, currentCell.positionY]); //visited cell
+
+  for (i = 0; i <= width * height; i++) {
+    // (function (i) {
+    //   setTimeout(function () {
+    // Notify when exit found
+    if (currentCell.positionX == endPos[0] && currentCell.positionY == endPos[1]) {
+      exit.solution = exit.path.slice(); //Make copy of the exit path
+      exit.found = true;
+      console.log("Exit Found");
+    }
+    //  Check avaiable direction from current cell
+    currentCell.checkNeighbour();
+    //   action when reach dead end
+    if (currentCell.validNeighbour.length == 0) {
+      moveBack = exit.path[exit.path.length - 1];
+      if (moveBack != undefined) {
+        currentCell.positionX > moveBack[0] ? (direction = "left") : (direction = "right");
+        currentCell.positionY > moveBack[0] ? (direction = "top") : (direction = "bottom");
+
+        currentCell.positionX = moveBack[0];
+        currentCell.positionY = moveBack[1];
+        // exit.fullPath.push(direction);
+        currentCell.checkNeighbour();
+        exit.path.pop();
+        i--;
+      } else {
+        console.log("exit path is empty");
+      }
+    } else {
+      //   Filter avaiable direction of current cell for exit Path)
+
+      //Pick random an exit from the
+      randomPick = Math.floor(Math.random() * currentCell.validNeighbour.length);
+      exit.direction = currentCell.validNeighbour[randomPick];
+      // when weight direction doesn't include the path
+      if (exit.direction == undefined) {
+        exit.direction = currentCell.validNeighbour[0];
+      } else {
+        a = document.getElementById(currentCell.id());
+        a.style[`border-${exit.direction}`] = "none";
+      }
+
+      //  Continue to create deadend
+
+      exit.path.push([currentCell.positionX, currentCell.positionY]); // solution array
+
+      // break the wall in front
+
+      currentCell.move(exit.direction); //Move following direction
+
+      exit.visited.push([currentCell.positionX, currentCell.positionY]); //visited array
+
+      a = document.getElementById(currentCell.id());
+      a.classList.add("visited");
+      a.style[`border-${exit.directionOpposite()}`] = "none"; //break the wall behind
+    }
+    //   }, interval * i);
+    // })(i);
+  }
+}
+// $(`cell_${startPos[0]}_${startPos[1]}`).style.backgroundColor = color.startCell;
+// $(`cell_${endPos[0]}_${endPos[1]}`).style.backgroundColor = color.finishCell;
+
+function showSolution() {
+  exit.solution.forEach((a) => {
+    $(`cell_${a[0]}_${a[1]}`).style.backgroundColor = color.path;
+  });
+}
 //#endregion End Function
 
 //#region  Objects
-let player = {
-  start: function () {
-    this.positionX = startPos[0];
-    this.positionY = startPos[1];
-  },
-
-  positionX: null,
-  positionY: null,
-};
-
-//  Construct shortest Exit path
 let exit = {
   direction: null,
   validExit: ["left", "right", "top", "bottom"],
@@ -154,11 +312,121 @@ let currentCell = {
   },
 };
 
+let player = {
+  start: function () {
+    this.positionX = startPos[0];
+    this.positionY = startPos[1];
+    this.icon = document.createElement("i");
+    this.icon.classList.add("fas");
+    this.icon.classList.add("fa-chess-king");
+    this.initiate = true;
+  },
+  positionX: null,
+  positionY: null,
+  icon: null,
+  totalMove: [],
+  availableMove: ["right", "left", "top", "bottom"],
+  validMove: [],
+  checkPath: function () {
+    this.validMove = [];
+    a = $(`cell_${player.positionX}_${player.positionY}`);
+    for (let i = 0; i <= 3; i++) {
+      if (a.style[`border-${this.availableMove[i]}`] == "none") {
+        this.validMove.push(this.availableMove[i]);
+        console.log(this.availableMove[i]);
+      }
+    }
+  },
+  show: function () {
+    if (this.initiate) {
+      $(`cell_${player.positionX}_${player.positionY}`).appendChild(this.icon);
+    } else {
+      console.log("Please Initiate Player");
+    }
+  },
+  hide: function () {
+    if (this.initiate) {
+      $(`cell_${player.positionX}_${player.positionY}`).removeChild(this.icon);
+    } else {
+      console.log("Please Initiate Player");
+    }
+  },
+  move: function (d) {
+    this.checkPath();
+    if (this.validMove.indexOf(d) >= 0) {
+      this.hide();
+      switch (d) {
+        case "top":
+          this.totalMove.push["top"];
+          this.positionY--;
+          upPressed = false;
+          break;
+        case "bottom":
+          this.totalMove.push["bottom"];
+          this.positionY++;
+          downPressed = false;
+          break;
+        case "left":
+          this.totalMove.push["left"];
+          this.positionX--;
+          leftPressed = false;
+          break;
+        case "right":
+          this.totalMove.push["right"];
+          this.positionX++;
+          rightPressed = false;
+          break;
+        default:
+          console("Invalid Move");
+          break;
+      }
+      this.show();
+      console.log(d);
+    }
+  },
+};
+
 //#endregion Object
 
+function play() {
+  $("mazeContainer").innerHTML = "";
+  createTable(width, height);
+  buildMaze();
+  player.start();
+  player.show();
+}
+
 //  Creat blank table
+$("mazeContainer").innerHTML = "";
 createTable(width, height);
+buildMaze();
+showSolution();
+//#endregion  End MAZE GENERATOR
+
 player.start();
+player.show();
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+checkscore = setInterval(() => {
+  if (rightPressed) {
+    player.move("right");
+  }
+  if (leftPressed) {
+    player.move("left");
+  }
+  if (upPressed) {
+    player.move("top");
+  }
+  if (downPressed) {
+    player.move("bottom");
+  }
+  if (player.positionX == endPos[0] && player.positionY == endPos[1]) {
+    clearInterval(checkscore);
+    alert("You Win");
+  }
+}, 100);
 
 //  Shortest path to the destination
 // let xmin = endPos[0] - startPos[0];
@@ -174,74 +442,5 @@ player.start();
 // exit.availablePath = shuffle(exit.availablePath);
 
 // initiate starting point
-currentCell.positionX = startPos[0];
-currentCell.positionY = startPos[1];
 
-$(currentCell.id()).classList.add("visited");
-
-exit.path.push([currentCell.positionX, currentCell.positionY]); // current correct cell
-exit.visited.push([currentCell.positionX, currentCell.positionY]); //visited cell
-
-for (i = 0; i <= width * height; i++) {
-  // Notify when exit found
-  if (currentCell.positionX == endPos[0] && currentCell.positionY == endPos[1]) {
-    exit.solution = exit.path.slice(); //Make copy of the exit path
-    exit.found = true;
-    console.log("Exit Found");
-  }
-  //  Check avaiable direction from current cell
-  currentCell.checkNeighbour();
-  //   action when reach dead end
-  if (currentCell.validNeighbour.length == 0) {
-    moveBack = exit.path[exit.path.length - 1];
-    if (moveBack != undefined) {
-      currentCell.positionX > moveBack[0] ? (direction = "left") : (direction = "right");
-      currentCell.positionY > moveBack[0] ? (direction = "top") : (direction = "bottom");
-
-      currentCell.positionX = moveBack[0];
-      currentCell.positionY = moveBack[1];
-      // exit.fullPath.push(direction);
-      currentCell.checkNeighbour();
-      exit.path.pop();
-      i--;
-    } else {
-      console.log("exit path is empty");
-    }
-  } else {
-    //   Filter avaiable direction of current cell for exit Path)
-
-    // Check for direction
-    randomPick = Math.floor(Math.random() * currentCell.validNeighbour.length); //Pick random an exit from the
-    exit.direction = currentCell.validNeighbour[randomPick];
-    // when weight direction doesn't include the path
-    if (exit.direction == undefined) {
-      exit.direction = currentCell.validNeighbour[0];
-    } else {
-      a = document.getElementById(currentCell.id());
-      a.style[`border-${exit.direction}`] = "none";
-    }
-
-    //  Continue to create deadend
-
-    exit.path.push([currentCell.positionX, currentCell.positionY]); // solution array
-
-    // break the wall in front
-
-    currentCell.move(exit.direction); //Move following direction
-
-    exit.visited.push([currentCell.positionX, currentCell.positionY]); //visited array
-
-    a = document.getElementById(currentCell.id());
-    a.classList.add("visited");
-    a.style[`border-${exit.directionOpposite()}`] = "none"; //break the wall behind
-  }
-}
-exit.solution.forEach((a) => {
-  $(`cell_${a[0]}_${a[1]}`).style.backgroundColor = color.path;
-});
-
-$(`cell_${startPos[0]}_${startPos[1]}`).style.backgroundColor = color.startCell;
-$(`cell_${endPos[0]}_${endPos[1]}`).style.backgroundColor = color.finishCell;
-
-//#endregion  End MAZE GENERATOR
 //  check exit.found false then re run the program
